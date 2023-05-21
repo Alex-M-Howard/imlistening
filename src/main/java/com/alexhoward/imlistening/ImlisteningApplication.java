9cfdb2c4afc8a51f1a058f7e484a2c1d197bed00
Alex Howard <alex-m-howard@pm.me>
2023-05-21 16:42:13 -0400

    Testing out webhooks

--- src/main/java/com/alexhoward/imlistening/ImlisteningApplication.java
+++ src/main/java/com/alexhoward/imlistening/ImlisteningApplication.java
@@ -21,6 +21,10 @@ import com.twilio.twiml.MessagingResponse;
 import com.twilio.twiml.TwiMLException;
 import com.twilio.twiml.messaging.*;
 import com.twilio.rest.api.v2010.account.Message;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.core.publisher.Mono;
 
 
 @SpringBootApplication
@@ -42,127 +46,248 @@ public class ImlisteningApplication {
 		}
 	}
 
-	@RestController
-	@RequestMapping("/api")
-	public class FrasierController {
+	 @RestController
+    @RequestMapping("/webhook")
+    public class WebhookController {
 
-		@PostMapping("/send")
-		public String send(@RequestBody InputData data) {
-			System.out.println(data.getUserInput());
-			try {
-				JsonArray messages = new JsonArray();
+        private final WebClient webClient;
 
-				JsonObject systemMessage = new JsonObject();
-				systemMessage.addProperty("role", "system");
-				systemMessage.addProperty("content",
-						"You are Frasier Crane from the TV show Frasier, and I am your brother Niles Crane. ");
-				messages.add(systemMessage);
+        public WebhookController(WebClient webClient) {
+            this.webClient = webClient;
+        }
 
-				JsonObject userMessage = new JsonObject();
-				userMessage.addProperty("role", "user");
-				userMessage.addProperty("content", data.getUserInput());
-				messages.add(userMessage);
+        @PostMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+        public Mono<ResponseEntity<String>> handleWebhook(
+                @RequestParam("Body") String body,
+                @RequestParam("From") String fromNumber
+        ) {
+            System.out.println("Received SMS from: " + fromNumber + ", Content: " + body);
 
-				JsonObject json = new JsonObject();
-				json.add("messages", messages);
-				json.addProperty("max_tokens", 150);
-				json.addProperty("model", "gpt-3.5-turbo");
+            // Create the input data object
+            InputData inputData = new InputData();
+            inputData.setUserInput(body);
 
-				HttpClient httpClient = HttpClients.createDefault();
-				HttpPost request = new HttpPost("https://api.openai.com/v1/chat/completions");
-				request.setHeader("Authorization", "Bearer " + System.getenv("OPENAI_API_KEY"));
-				request.setHeader("Content-Type", "application/json");
+            // Call the send function using WebClient
+            return send(inputData)
+                    .map(response -> ResponseEntity.ok(response));
+        }
 
-				StringEntity requestBody = new StringEntity(json.toString(), "UTF-8");
-				request.setEntity(requestBody);
+        private Mono<String> send(InputData data) {
+            System.out.println(data.getUserInput());
+            try {
+                // Construct the request body
+                JsonArray messages = new JsonArray();
 
-				HttpResponse response = httpClient.execute(request);
-				HttpEntity entity = response.getEntity();
+                JsonObject systemMessage = new JsonObject();
+                systemMessage.addProperty("role", "system");
+                systemMessage.addProperty("content",
+                        "You are Frasier Crane from the TV show Frasier, and I am your brother Niles Crane. ");
+                messages.add(systemMessage);
 
-				String responseBody = EntityUtils.toString(entity, "UTF-8");
-				return responseBody;
+                JsonObject userMessage = new JsonObject();
+                userMessage.addProperty("role", "user");
+                userMessage.addProperty("content", data.getUserInput());
+                messages.add(userMessage);
 
-			} catch (Exception e) {
-				e.printStackTrace();
-				return "Error";
-			}
+                JsonObject json = new JsonObject();
+                json.add("messages", messages);
+                json.addProperty("max_tokens", 150);
+                json.addProperty("model", "gpt-3.5-turbo");
 
-		}
+                // Make the HTTP POST request using WebClient
+                return webClient.post()
+                        .uri("https://api.openai.com/v1/chat/completions")
+                        .header("Authorization", "Bearer " + System.getenv("OPENAI_API_KEY"))
+                        .header("Content-Type", "application/json")
+                        .body(BodyInserters.fromValue(json.toString()))
+                        .retrieve()
+                        .bodyToMono(String.class);
 
-		@PostMapping("/sms")
-		public ResponseEntity<String> handleSms(@RequestParam("Body") String body,
-				@RequestParam("From") String fromNumber) {
-			try {
-				System.out.println("Received SMS from: " + fromNumber + ", Content: " + body);
-
-				InputData inputData = new InputData();
-				inputData.setUserInput(body);
-
-				String openAiResponse = this.send(inputData);
-
-				SmsSender.sendSms(openAiResponse, fromNumber);
-
-				Body bodyResponse = new Body.Builder("Your message has been processed.").build();
-				com.twilio.twiml.messaging.Message sms = new com.twilio.twiml.messaging.Message.Builder()
-						.body(bodyResponse)
-						.build();
-				MessagingResponse twiml = new MessagingResponse.Builder().message(sms).build();
-
-				try {
-					return ResponseEntity.ok().contentType(MediaType.APPLICATION_XML).body(twiml.toXml());
-				} catch (TwiMLException e) {
-					return ResponseEntity.status(500).body("Error generating TwiML.");
-				}
-
-			} catch (Exception e) {
-				e.printStackTrace();
-				return ResponseEntity.status(500).body("Error processing SMS.");
-			}
-		}
-	
-		@GetMapping("/test")
-    public ResponseEntity<String> testRoute() {
-        return ResponseEntity.ok("This is a test route.");
+            } catch (Exception e) {
+                e.printStackTrace();
+                return Mono.just("Error");
+            }
+        }
     }
-	
-	}
-	
-	public static class SmsSender {
-    private static final String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID");
-    private static final String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
-    private static final String TWILIO_NUMBER = System.getenv("TWILIO_PHONE_NUMBER");
-
-    public static void sendSms(String body, String toNumber) {
-        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
-
-        Message message = Message.creator(
-            new PhoneNumber(toNumber),
-            new PhoneNumber(TWILIO_NUMBER),
-            body)
-            .create();
-
-        System.out.println("Message sent. ID: " + message.getSid());
-    }
-	}
-
-
-	@RestController
-	@RequestMapping("/webhook")
-	public class WebhookController {
-
-		@PostMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-		public ResponseEntity<String> handleWebhook(
-			@RequestParam("Body") String body,
-			@RequestParam("From") String fromNumber
-		) {
-
-			System.out.println("Received SMS from: " + fromNumber + ", Content: " + body);
-
-			return ResponseEntity.ok(fromNumber + " " + body);
-		}
-	}
-
 }
+
+
+// 	@RestController
+// 	@RequestMapping("/api")
+// 	public class FrasierController {
+
+// 		@PostMapping("/send")
+// 		public String send(@RequestBody InputData data) {
+// 			System.out.println(data.getUserInput());
+// 			try {
+// 				JsonArray messages = new JsonArray();
+
+// 				JsonObject systemMessage = new JsonObject();
+// 				systemMessage.addProperty("role", "system");
+// 				systemMessage.addProperty("content",
+// 						"You are Frasier Crane from the TV show Frasier, and I am your brother Niles Crane. ");
+// 				messages.add(systemMessage);
+
+// 				JsonObject userMessage = new JsonObject();
+// 				userMessage.addProperty("role", "user");
+// 				userMessage.addProperty("content", data.getUserInput());
+// 				messages.add(userMessage);
+
+// 				JsonObject json = new JsonObject();
+// 				json.add("messages", messages);
+// 				json.addProperty("max_tokens", 150);
+// 				json.addProperty("model", "gpt-3.5-turbo");
+
+// 				HttpClient httpClient = HttpClients.createDefault();
+// 				HttpPost request = new HttpPost("https://api.openai.com/v1/chat/completions");
+// 				request.setHeader("Authorization", "Bearer " + System.getenv("OPENAI_API_KEY"));
+// 				request.setHeader("Content-Type", "application/json");
+
+// 				StringEntity requestBody = new StringEntity(json.toString(), "UTF-8");
+// 				request.setEntity(requestBody);
+
+// 				HttpResponse response = httpClient.execute(request);
+// 				HttpEntity entity = response.getEntity();
+
+// 				String responseBody = EntityUtils.toString(entity, "UTF-8");
+// 				return responseBody;
+
+// 			} catch (Exception e) {
+// 				e.printStackTrace();
+// 				return "Error";
+// 			}
+
+// 		}
+
+// 		// @PostMapping("/sms")
+// 		// public ResponseEntity<String> handleSms(@RequestParam("Body") String body,
+// 		// 		@RequestParam("From") String fromNumber) {
+// 		// 	try {
+// 		// 		System.out.println("Received SMS from: " + fromNumber + ", Content: " + body);
+
+// 		// 		InputData inputData = new InputData();
+// 		// 		inputData.setUserInput(body);
+
+// 		// 		String openAiResponse = this.send(inputData);
+
+// 		// 		SmsSender.sendSms(openAiResponse, fromNumber);
+
+// 		// 		Body bodyResponse = new Body.Builder("Your message has been processed.").build();
+// 		// 		com.twilio.twiml.messaging.Message sms = new com.twilio.twiml.messaging.Message.Builder()
+// 		// 				.body(bodyResponse)
+// 		// 				.build();
+// 		// 		MessagingResponse twiml = new MessagingResponse.Builder().message(sms).build();
+
+// 		// 		try {
+// 		// 			return ResponseEntity.ok().contentType(MediaType.APPLICATION_XML).body(twiml.toXml());
+// 		// 		} catch (TwiMLException e) {
+// 		// 			return ResponseEntity.status(500).body("Error generating TwiML.");
+// 		// 		}
+
+// 		// 	} catch (Exception e) {
+// 		// 		e.printStackTrace();
+// 		// 		return ResponseEntity.status(500).body("Error processing SMS.");
+// 		// 	}
+// 		// }
+	
+// 		@GetMapping("/test")
+//     public ResponseEntity<String> testRoute() {
+//         return ResponseEntity.ok("This is a test route.");
+//     }
+	
+// 	}
+	
+// 	// public static class SmsSender {
+//   //   private static final String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID");
+//   //   private static final String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
+//   //   private static final String TWILIO_NUMBER = System.getenv("TWILIO_PHONE_NUMBER");
+
+//   //   public static void sendSms(String body, String toNumber) {
+//   //       Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+
+//   //       Message message = Message.creator(
+//   //           new PhoneNumber(toNumber),
+//   //           new PhoneNumber(TWILIO_NUMBER),
+//   //           body)
+//   //           .create();
+
+//   //       System.out.println("Message sent. ID: " + message.getSid());
+//   //   }
+// 	// }
+
+
+// 	@RestController
+// 	@RequestMapping("/webhook")
+// 	public class WebhookController {
+
+// 			private final RestTemplate restTemplate;
+
+// 			public WebhookController(RestTemplate restTemplate) {
+// 					this.restTemplate = restTemplate;
+// 			}
+
+// 			@PostMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+// 			public ResponseEntity<String> handleWebhook(
+// 					@RequestParam("Body") String body,
+// 					@RequestParam("From") String fromNumber
+// 			) {
+// 					System.out.println("Received SMS from: " + fromNumber + ", Content: " + body);
+
+// 					// Create the input data object
+// 					InputData inputData = new InputData();
+// 					inputData.setUserInput(body);
+
+// 					// Call the send function
+// 					String response = send(inputData);
+
+// 					// Return the response from the send function
+// 					return ResponseEntity.ok(response);
+// 			}
+
+// 			private String send(InputData data) {
+// 					System.out.println(data.getUserInput());
+// 					try {
+// 							JsonArray messages = new JsonArray();
+
+// 							JsonObject systemMessage = new JsonObject();
+// 							systemMessage.addProperty("role", "system");
+// 							systemMessage.addProperty("content",
+// 											"You are Frasier Crane from the TV show Frasier, and I am your brother Niles Crane. ");
+// 							messages.add(systemMessage);
+
+// 							JsonObject userMessage = new JsonObject();
+// 							userMessage.addProperty("role", "user");
+// 							userMessage.addProperty("content", data.getUserInput());
+// 							messages.add(userMessage);
+
+// 							JsonObject json = new JsonObject();
+// 							json.add("messages", messages);
+// 							json.addProperty("max_tokens", 150);
+// 							json.addProperty("model", "gpt-3.5-turbo");
+
+// 							HttpClient httpClient = HttpClients.createDefault();
+// 							HttpPost request = new HttpPost("https://api.openai.com/v1/chat/completions");
+// 							request.setHeader("Authorization", "Bearer " + System.getenv("OPENAI_API_KEY"));
+// 							request.setHeader("Content-Type", "application/json");
+
+// 							StringEntity requestBody = new StringEntity(json.toString(), "UTF-8");
+// 							request.setEntity(requestBody);
+
+// 							HttpResponse response = httpClient.execute(request);
+// 							HttpEntity entity = response.getEntity();
+
+// 							String responseBody = EntityUtils.toString(entity, "UTF-8");
+// 							return responseBody;
+
+// 					} catch (Exception e) {
+// 							e.printStackTrace();
+// 							return "Error";
+// 					}
+// 			}
+// 	}
+
+
+// }
 
 
 
